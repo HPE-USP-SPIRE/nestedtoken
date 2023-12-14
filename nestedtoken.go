29f4542a2b1b7bbe9ca0d5410daaf0af17866072
marques-ma <marcoantonio.marques@gmail.com>
2023-12-13 21:44:16 -0300

    add support to schoco

--- nestedtoken.go
+++ nestedtoken.go
@@ -1,7 +1,7 @@
 package nestedtoken
 
-// This package contains the main structs and functions to implement the nested token model using ID-mode, as specified in XYZ (UPDATE)
-// Anonymous mode can be found in a separated package (INSERT REFERENCE)
+// This package contains the main structs and functions to implement the nested token model as specified in 
+// https://docs.google.com/document/d/1nQYV4wf8wiogpxboIVbwtFZyZjLNRejyguHoGZIZLQM
 
 import (
 	"fmt"
@@ -13,6 +13,8 @@ import (
 	"crypto/x509"
 	"crypto/ecdsa"
 	"log"
+	"github.com/hpe-usp-spire/schoco"
+	"go.dedis.ch/kyber/v3"
 )
 
 // The token must contain a Payload and a Signature. 
@@ -23,7 +25,7 @@ type Token struct {
 	Signature	[]byte					`json:"signature"`
 }
 
-// The mandatory claims are pre-defined. Any other info to be added must be inserted in Data.
+// The mandatory claims are pre-defined. Any other info to be added must be inserted into Data.
 type Payload struct {
 	Ver 		int8					`json:"ver,omitempty"`
 	Iat			int64					`json:"iat,omitempty"`
@@ -75,7 +77,10 @@ func Decode(encToken string) (*Token, error) {
 }
 
 // Create a new Token (a.k.a. sign)
-func Create(newPayload *Payload, key crypto.Signer) (string, error) {
+// If key not informed, uses randomkey and Anon mode EDDSA
+// otherwise, use ID-mode with ECDSA
+// Version follows specification document, where 0 = ID mode with ECDSA and 1 = SchoCo
+func Create(newPayload *Payload, version int8, key interface{}) (string, error) {
 
 	newToken := &Token{
 		Payload:	newPayload,	
@@ -87,12 +92,39 @@ func Create(newPayload *Payload, key crypto.Signer) (string, error) {
 		return "", fmt.Errorf("Error generating json: %v\n", err)
 	} 
 
-	// Sign Token
-	hash 	:= sha256.Sum256(tmpToSign)
-	s, err := key.Sign(rand.Reader, hash[:], crypto.SHA256)
-	if err != nil {
-		return "", fmt.Errorf("Error generating signed assertion: %v\n", err)
-	} 
+	// Choose between ID / SchoCo
+	var s []byte
+    switch version {
+	case 0:
+		// Assume that key is ID mode and ECDSA
+		// TODO generalize
+		// Sign Token
+		ecdsaKey, ok := key.(*ecdsa.PrivateKey)
+		if !ok {
+			return "", fmt.Errorf("key is not an ECDSA private key")
+		}
+
+		hash 	:= sha256.Sum256(tmpToSign)
+		s, err = ecdsaKey.Sign(rand.Reader, hash[:], crypto.SHA256)
+		if err != nil {
+			return "", fmt.Errorf("Error generating signed assertion: %v\n", err)
+		} 
+
+	case 1:
+		// Uses schoco
+		eddsaKey, ok := key.(kyber.Scalar)
+		if !ok {
+			return "", fmt.Errorf("key is not an EdDSA private key")
+		}
+
+
+		// Sign with new key
+		sig := schoco.StdSign(fmt.Sprintf("%s", tmpToSign), eddsaKey)
+		s, err = sig.ToByte()
+		if err != nil {
+			return "", fmt.Errorf("Error generating signed assertion: %v\n", err)
+		} 
+	}
 
 	// Set Token signature
 	newToken.Signature = s
@@ -107,43 +139,102 @@ func Create(newPayload *Payload, key crypto.Signer) (string, error) {
 }
 
 // Extend an existing token with a new payload, and sign using provided key
-func Extend(token *Token, newPayload *Payload, key crypto.Signer) (string, error) {
+// If key not informed, uses anon mode validation (SchoCo)
+// otherwise, use ID-mode with ECDSA
+func Extend(token *Token, newPayload *Payload, version int8, key ...interface{}) (string, error) {
 	// TODO: Modify the payload struct to support custom claims (maybe using map[string]{interface})
 	// Create the extended Token structure
+	
+	// Choose between ID / SchoCo
+	var s []byte
+	var newToken *Token
+	var outToken string
+    switch version {
+	case 0:
+		// Uses ECDSA
+		// TODO generalize
+		// Sign Token
 
-	newToken := &Token{
-		Nested:		token,
-		Payload:	newPayload,	
-	}
+		// Define the new struct
+		newToken = &Token{
+			Nested:		token,
+			Payload:	newPayload,	
+		}
 
-	// Marshal to JSON
-	tmpToSign, err := json.Marshal(newToken)
-	if err != nil {
-		return "", fmt.Errorf("Error generating json: %v\n", err)
-	} 
+		// Marshal to JSON
+		tmpToSign, err := json.Marshal(newToken)
+		if err != nil {
+			return "", fmt.Errorf("Error generating json: %v\n", err)
+		} 
 
-	// Sign extlSVID
-	hash 	:= sha256.Sum256(tmpToSign)
-	s, err := key.Sign(rand.Reader, hash[:], crypto.SHA256)
-	if err != nil {
-		return "", fmt.Errorf("Error generating signed assertion: %v\n", err)
-	} 
+		ecdsaKey, ok := key[0].(*ecdsa.PrivateKey)
+		if !ok {
+			return "", fmt.Errorf("key is not an ECDSA private key")
+		}
+			
+		hash 	:= sha256.Sum256(tmpToSign)
+		s, err = ecdsaKey.Sign(rand.Reader, hash[:], crypto.SHA256)
+		if err != nil {
+			return "", fmt.Errorf("Error generating signed assertion: %v\n", err)
+		} 
 
-	// Set extToken signature
-	newToken.Signature = s
+		// Set extToken signature
+		newToken.Signature = s
 
-	// Encode signed Token
-	outToken, err := Encode(newToken)
-	if err != nil {
-		return "", fmt.Errorf("Error encoding Token: %v\n", err)
-	} 
+		// Encode signed Token
+		outToken, err = Encode(newToken)
+		if err != nil {
+			return "", fmt.Errorf("Error encoding Token: %v\n", err)
+		} 
 
+	case 1:
+		// Uses schoco
+
+		// Convert []byte to sig
+		sig, err := schoco.ByteToSignature(token.Signature)
+
+		// extract key from signature
+		aggKey, partSig := sig.ExtractAggKey()
+
+		// convert partsig to byte
+		partSigBytes, err := schoco.PointToByte(partSig)
+		if err != nil {
+			return "", fmt.Errorf("Error conveting point to byte: %v", err)
+		} 
+		token.Signature = partSigBytes
+
+		// Define the new struct
+		newToken = &Token{
+			Nested:		token,
+			Payload:	newPayload,	
+		}
+
+		// Marshal to JSON
+		tmpToSign, err := json.Marshal(newToken)
+		if err != nil {
+			return "", fmt.Errorf("Error generating json: %v\n", err)
+		} 
+
+		// Sign with aggKey key
+		newSig := schoco.StdSign(fmt.Sprintf("%s", tmpToSign), aggKey)
+		s, err = newSig.ToByte()
+		if err != nil {
+			return "", fmt.Errorf("Error generating signed assertion: %v\n", err)
+		} 
+		// Set extToken signature
+		newToken.Signature = s
+
+		// Encode signed Token
+		outToken, err = Encode(newToken)
+		if err != nil {
+			return "", fmt.Errorf("Error encoding Token: %v\n", err)
+		} 
+		}
 	return outToken, nil
-
 }
 
 // Validate the given Token. 
-// TODO: retrieve the public key from iss.id
+// TODO: retrieve the public key from iss.id (?)
 // TODO: validate iss.id (actually, all places that use .id must validate the root. 
 // We can assume using bundle to validate any root)
 // 
@@ -156,20 +247,54 @@ func Extend(token *Token, newPayload *Payload, key crypto.Signer) (string, error
 // 2 - Get PK from iss.id.nested.payload.sub.pk
 // 4 - Validate also issuer bundle
 // 
-func Validate(token *Token, bundle *Token) (bool, error) {
+func Validate(token *Token, version int8, bundle ...*Token) (bool, error) {
 
-	for (token.Nested != nil) {
+    switch version {
+	case 0:
+		// Validate ID Mode with ECDSA
 
-		// Check Aud -> Iss link
-		if token.Payload.Iss.CN != token.Nested.Payload.Aud.CN {
-			return false, fmt.Errorf("Aud -> Iss link validation failed\n")
+		for (token.Nested != nil) {
+
+			// Check Aud -> Iss link
+			if token.Payload.Iss.CN != token.Nested.Payload.Aud.CN {
+				return false, fmt.Errorf("Aud -> Iss link validation failed\n")
+			}
+			log.Printf("Aud -> Iss link validation successful!\n")
+
+			// Marshal the Token struct into JSON
+			tmpToken := &Token{
+				Nested:		token.Nested,
+				Payload:	token.Payload,
+			}
+			tokenJSON, err := json.Marshal(tmpToken)
+			if err != nil {
+			return false, fmt.Errorf("error marshaling Token to JSON: %v\n", err)
+			}
+			hash 	:= sha256.Sum256(tokenJSON)
+
+			// Parse the public key
+			issPk, err := x509.ParsePKIXPublicKey(token.Payload.Iss.PK)
+			if err != nil {
+				return false, fmt.Errorf("Failed to parse public key: %v\n", err)
+			}
+
+			// validate the signature
+			log.Printf("Verifying signature created by %s\n", token.Payload.Iss.CN)
+			verify := ecdsa.VerifyASN1(issPk.(*ecdsa.PublicKey), hash[:], token.Signature)
+			if verify == false {
+				log.Printf("\nSignature validation failed!\n\n")
+				return false, nil
+			}
+			log.Printf("Signature validation successful!\n")
+
+			// jump to nested token
+			token = token.Nested
 		}
-		log.Printf("Aud -> Iss link validation successful!\n")
 
-		// Marshal the Token struct into JSON
+		// reached the inner most Token. 
+		// Marshal the bundle struct into JSON
 		tmpToken := &Token{
-			Nested:		token.Nested,
-			Payload:	token.Payload,
+			Payload:	bundle[0].Payload,
 		}
 		tokenJSON, err := json.Marshal(tmpToken)
 		if err != nil {
@@ -178,47 +303,85 @@ func Validate(token *Token, bundle *Token) (bool, error) {
 		hash 	:= sha256.Sum256(tokenJSON)
 
 		// Parse the public key
-		issPk, err := x509.ParsePKIXPublicKey(token.Payload.Iss.PK)
+		subPk, err := x509.ParsePKIXPublicKey(bundle[0].Payload.Sub.PK)
 		if err != nil {
 			return false, fmt.Errorf("Failed to parse public key: %v\n", err)
 		}
+		// log.Printf("Public key to be used: %s", issPk)
 
-		// validate the signature
-		log.Printf("Verifying signature created by %s\n", token.Payload.Iss.CN)
-		verify := ecdsa.VerifyASN1(issPk.(*ecdsa.PublicKey), hash[:], token.Signature)
+		log.Printf("Verifying signature created by %s\n", bundle[0].Payload.Sub.CN)
+		verify := ecdsa.VerifyASN1(subPk.(*ecdsa.PublicKey), hash[:], token.Signature)
 		if verify == false {
-			log.Printf("\nSignature validation failed!\n\n")
+			log.Printf("\nBundle signature validation failed!\n\n")
 			return false, nil
 		}
-		log.Printf("Signature validation successful!\n")
 
-		// jump to nested token
-		token = token.Nested
-	}
+	case 1:
+		// Validate anon mode with SchoCo
+		// Collect partial sigs and messages from all nested tokens
+		var	setPartSig	[]kyber.Point
+		var lastSig		schoco.Signature
+		var	setMsg		[]string
+		var i = 0
+		for (token.Nested != nil) {
+			tmpToken := &Token{
+				Nested:		token.Nested,
+				Payload:	token.Payload,
+			}
 
-	// reached the inner most Token. 
-	// Marshal the bundle struct into JSON
-	tmpToken := &Token{
-		Payload:	bundle.Payload,
-	}
-	tokenJSON, err := json.Marshal(tmpToken)
-	if err != nil {
-	return false, fmt.Errorf("error marshaling Token to JSON: %v\n", err)
-	}
-	hash 	:= sha256.Sum256(tokenJSON)
+			// Marshal to JSON
+			tokenJSON, err := json.Marshal(tmpToken)
+			if err != nil {
+				log.Printf("Error marshaling Token to JSON: %v\n", err)
+			}
+			// Collect signed message
+			setMsg = append(setMsg, fmt.Sprintf("%s", tokenJSON))
 
-	// Parse the public key
-	subPk, err := x509.ParsePKIXPublicKey(bundle.Payload.Sub.PK)
-	if err != nil {
-		return false, fmt.Errorf("Failed to parse public key: %v\n", err)
-	}
-	// log.Printf("Public key to be used: %s", issPk)
+			// Collect signature
+			if i == 0 {
+				lastSig, err = schoco.ByteToSignature(token.Signature)
+				if err != nil {
+					log.Printf("Error converting byte to signature: %v\n", err)
+				}
+			} else {
+				setPartSig[i], err = schoco.ByteToPoint(token.Signature)
+				if err != nil {
+					log.Printf("Error converting byte to Point: %v\n", err)
+				}
+			}
 
-	log.Printf("Verifying signature created by %s\n", bundle.Payload.Sub.CN)
-	verify := ecdsa.VerifyASN1(subPk.(*ecdsa.PublicKey), hash[:], token.Signature)
-	if verify == false {
-		log.Printf("\nBundle signature validation failed!\n\n")
-		return false, nil
+			// jump to nested token
+			token = token.Nested
+			i++
+		}
+
+		// reach most inner token
+		// Collect msg
+		tmpToken := &Token{
+			Payload:	token.Payload,
+		}
+		tokenJSON, err := json.Marshal(tmpToken)
+		if err != nil {
+			log.Printf("error marshaling Token to JSON: %v\n", err)
+		}
+		setMsg = append(setMsg, fmt.Sprintf("%s", tokenJSON))
+
+		// collect partsig
+		tmpPartSig, err := schoco.ByteToPoint(token.Signature)
+		if err != nil {
+			log.Printf("Error converting byte to Point: %v\n", err)
+		}
+		setPartSig = append(setPartSig, tmpPartSig)
+
+		// collect root pk
+		rootPK, err := schoco.ByteToPoint(token.Payload.Iss.PK)
+		if err != nil {
+			log.Printf("Error converting byte to Point: %v\n", err)
+		}
+
+		if !schoco.Verify(rootPK, setMsg, setPartSig, lastSig)	{
+			log.Printf("Validate with schoco.Verify failed!\n")
+		}
 	}
 
 	return true, nil
--- nestedtoken_test.go
+++ nestedtoken_test.go
@@ -12,6 +12,7 @@ import (
 	"crypto/x509"
 
 	"github.com/hpe-usp-spire/nestedtoken"
+	"github.com/hpe-usp-spire/schoco"
 )
 
 func generateECDSAKey(t *testing.T) *ecdsa.PrivateKey {
@@ -24,182 +25,283 @@ func generateECDSAKey(t *testing.T) *ecdsa.PrivateKey {
 
 	return privateKey
 }
+func TestAll(t *testing.T){
 
-func TestEncoding(t *testing.T) {
-	// Creating a sample Token
-	sampleToken := &nestedtoken.Token{
-		Payload: &nestedtoken.Payload{
-			Ver: 1,
+	t.Run("Test Encoding", func(t *testing.T) {
+		// Creating a sample Token
+		sampleToken := &nestedtoken.Token{
+			Payload: &nestedtoken.Payload{
+				Ver: 0,
+				Iat: time.Now().Unix(),
+				Iss: &nestedtoken.IDClaim{
+					CN: "example.org",
+					PK: []byte("Issuer_public_key"),
+					ID: nil,
+				},
+				Aud: &nestedtoken.IDClaim{
+					CN: "audience.example",
+					PK: []byte("audience_public_key"),
+					ID: nil,
+				},
+				Data: map[string]interface{}{
+					"custom_claim": "custom_value",
+				},
+			},
+		}
+
+		// Encode the sample Token
+		encodedToken, err := nestedtoken.Encode(sampleToken)
+		if err != nil {
+			t.Errorf("Encode failed: %v", err)
+		}
+		t.Logf("Encoded token: %s\n", encodedToken)
+
+		// Decode the encoded token to verify integrity
+		decodedTokenJSON, err := base64.RawURLEncoding.DecodeString(encodedToken)
+		if err != nil {
+			t.Errorf("Error decoding token: %v", err)
+		}
+
+		var decodedToken nestedtoken.Token
+		err = json.Unmarshal(decodedTokenJSON, &decodedToken)
+		if err != nil {
+			t.Errorf("Error unmarshaling token: %v", err)
+		}
+
+		// Verify that the decodedToken matches the original sampleToken
+		// Usage of reflect is interesting when dealing with complex structs
+		if !reflect.DeepEqual(sampleToken, &decodedToken) {
+			t.Error("Decoded token does not match the original token")
+		}
+	})
+
+	t.Run("Test ECDSA single token creation and validation", func(t *testing.T) {
+		privateKey := generateECDSAKey(t)
+
+		pubkey, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+		if err != nil {
+			t.Fatalf("Failed to marshal public key: %v", err)
+		}
+
+		payload :=  &nestedtoken.Payload{
+			Ver: 0,
 			Iat: time.Now().Unix(),
 			Iss: &nestedtoken.IDClaim{
 				CN: "example.org",
-				PK: []byte("Issuer_public_key"),
-				ID: nil,
+				PK: pubkey,
 			},
 			Aud: &nestedtoken.IDClaim{
 				CN: "audience.example",
 				PK: []byte("audience_public_key"),
-				ID: nil,
+			},
+			Sub:  &nestedtoken.IDClaim{
+				CN: "example.org",
+				PK: pubkey,
 			},
 			Data: map[string]interface{}{
 				"custom_claim": "custom_value",
 			},
-		},
-	}
+		}
 
-	// Encode the sample Token
-	encodedToken, err := nestedtoken.Encode(sampleToken)
-	if err != nil {
-		t.Errorf("Encode failed: %v", err)
-	}
-	t.Logf("Encoded token: %s\n", encodedToken)
+		createdToken, err := nestedtoken.Create(payload, 0, privateKey)
+		if err != nil {
+			t.Fatalf("Failed to create token: %v", err)
+		}
+		t.Logf("Created token: %s\n", createdToken)
 
-	// Decode the encoded token to verify integrity
-	decodedTokenJSON, err := base64.RawURLEncoding.DecodeString(encodedToken)
-	if err != nil {
-		t.Errorf("Error decoding token: %v", err)
-	}
+		decodedToken, err := nestedtoken.Decode(createdToken)
+		if err != nil {
+			t.Fatalf("Failed to decode token: %v", err)
+		}
 
-	var decodedToken nestedtoken.Token
-	err = json.Unmarshal(decodedTokenJSON, &decodedToken)
-	if err != nil {
-		t.Errorf("Error unmarshaling token: %v", err)
-	}
+		// validate token
+		valid, err := nestedtoken.Validate(decodedToken, 0, decodedToken)
+		if err != nil {
+			t.Fatalf("Failed to decode token: %v", err)
+		}
+		if valid != true {
+			t.Fatalf("Failed to validate token")
+		}
 
-	// Verify that the decodedToken matches the original sampleToken
-	// Usage of reflect is interesting when dealing with complex structs
-	if !reflect.DeepEqual(sampleToken, &decodedToken) {
-		t.Error("Decoded token does not match the original token")
-	}
+	})
+
+	t.Run("Test ECDSA extend and validate", func(t *testing.T) {
+		// Generating a sample token for testing
+		privateKey := generateECDSAKey(t)
+
+		pubkey, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+		if err != nil {
+			t.Fatalf("Failed to marshal public key: %v", err)
+		}
+
+		payload := &nestedtoken.Payload{
+			Ver: 1,
+			Iat: time.Now().Unix(),
+			Iss: &nestedtoken.IDClaim{
+				CN: "Workload A",
+				PK: pubkey,
+			},
+			Aud: &nestedtoken.IDClaim{
+				CN: "Workload B",
+				PK: []byte("audience_public_key"),
+			},
+			Sub: &nestedtoken.IDClaim{
+				CN: "Workload A",
+				PK: pubkey,
+			},
+			Data: map[string]interface{}{
+				"custom_claim": "custom_value",
+			},
+		}
+
+		createdToken, err := nestedtoken.Create(payload, 0, privateKey)
+		if err != nil {
+			t.Fatalf("Failed to create token: %v", err)
+		}
+		t.Logf("Created token: %s\n", createdToken)
+
+		decodedToken, err := nestedtoken.Decode(createdToken)
+		if err != nil {
+			t.Fatalf("Failed to decode token: %v", err)
+		}
+
+		// Prepare a new payload
+		newPayload := &nestedtoken.Payload{
+			Ver: 2,
+			Iat: time.Now().Unix(),
+			Iss: &nestedtoken.IDClaim{
+				CN: "Workload B",
+				PK: pubkey,
+			},
+			Aud: &nestedtoken.IDClaim{
+				CN: "Workload C",
+				PK: []byte("audience_public_key"),
+			},
+			Data: map[string]interface{}{
+				"updated_claim": "updated_value",
+			},
+		}
+
+		// Extend the token
+		extendedToken, err := nestedtoken.Extend(decodedToken, newPayload, 0, privateKey)
+		if err != nil {
+			t.Fatalf("Failed to extend token: %v", err)
+		}
+		t.Logf("Extended token: %s\n", extendedToken)
+
+		// Decode the extended token
+		decodedExtendedToken, err := nestedtoken.Decode(extendedToken)
+		if err != nil {
+			t.Fatalf("Failed to decode extended token: %v", err)
+		}
+
+		// validate token
+		valid, err := nestedtoken.Validate(decodedExtendedToken, 0, decodedToken)
+		if err != nil {
+			t.Fatalf("Failed to decode token: %v", err)
+		}
+		if valid != true {
+			t.Fatalf("Failed to validate token")
+		}
+
+	})
+
+	t.Run("Test SchoCo token creation", func(t *testing.T) {
+
+		// creates random keypair
+		secretKey, publicKey := schoco.RandomKeyPair()
+		// convert publicKey to byte
+		PubKeyBytes, err := schoco.PointToByte(publicKey)
+		if err != nil {
+			t.Fatalf("Error conveting point to byte: %v", err)
+		} 
+
+		payload :=  &nestedtoken.Payload{
+			Ver: 1,
+			Iat: time.Now().Unix(),
+			Iss: &nestedtoken.IDClaim{
+				PK: PubKeyBytes,
+			},
+			Data: map[string]interface{}{
+				"custom_claim": "custom_value",
+			},
+		}
+
+		// Create token using EdDSA key
+		createdToken, err := nestedtoken.Create(payload, 1, secretKey)
+		if err != nil {
+			t.Fatalf("Failed to create token: %v", err)
+		}
+		t.Logf("Created token: %s\n", createdToken)
+
+	})
+
+	t.Run("Test Schoco creation and extension", func(t *testing.T) {	
+	
+		// creates random keypair
+		secretKey, publicKey := schoco.RandomKeyPair()
+
+		// convert publicKey to byte
+		PubKeyBytes, err := schoco.PointToByte(publicKey)
+		if err != nil {
+			t.Fatalf("Error conveting point to byte: %v", err)
+		} 
+
+		payload :=  &nestedtoken.Payload{
+			Ver: 1,
+			Iat: time.Now().Unix(),
+			Iss: &nestedtoken.IDClaim{
+				PK: PubKeyBytes,
+			},
+			Data: map[string]interface{}{
+				"custom_claim": "custom_value",
+			},
+		}
+
+		// Create token using EdDSA key
+		createdToken, err := nestedtoken.Create(payload, 1, secretKey)
+		if err != nil {
+			t.Fatalf("Failed to create token: %v", err)
+		}
+		t.Logf("Created token: %s\n", createdToken)
+
+		decodedToken, err := nestedtoken.Decode(createdToken)
+		if err != nil {
+			t.Fatalf("Failed to decode token: %v", err)
+		}
+
+		// Prepare a new payload
+		newPayload :=  &nestedtoken.Payload{
+			Ver: 1,
+			Iat: time.Now().Unix(),
+			Data: map[string]interface{}{
+				"newcustom_claim": "other_value",
+			},
+		}
+
+		// Extend the token
+		extendedToken, err := nestedtoken.Extend(decodedToken, newPayload, 1)
+		if err != nil {
+			t.Fatalf("Failed to extend token: %v", err)
+		}
+		t.Logf("Extended token: %s\n", extendedToken)
+
+		// Decode the extended token
+		decodedExtendedToken, err := nestedtoken.Decode(extendedToken)
+		if err != nil {
+			t.Fatalf("Failed to decode extended token: %v", err)
+		}
+
+		// validate token
+		valid, err := nestedtoken.Validate(decodedExtendedToken, 1, decodedToken)
+		if err != nil {
+			t.Fatalf("Failed to decode token: %v", err)
+		}
+		if valid != true {
+			t.Fatalf("Failed to validate token")
+		}
+
+	})
 }
 
-func TestSingleTokenValidation(t *testing.T) {
-	privateKey := generateECDSAKey(t)
 
-	pubkey, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
-	if err != nil {
-		t.Fatalf("Failed to marshal public key: %v", err)
-	}
-
-	payload :=  &nestedtoken.Payload{
-		Ver: 1,
-		Iat: time.Now().Unix(),
-		Iss: &nestedtoken.IDClaim{
-			CN: "example.org",
-			PK: pubkey,
-		},
-		Aud: &nestedtoken.IDClaim{
-			CN: "audience.example",
-			PK: []byte("audience_public_key"),
-		},
-		Sub:  &nestedtoken.IDClaim{
-			CN: "example.org",
-			PK: pubkey,
-		},
-		Data: map[string]interface{}{
-			"custom_claim": "custom_value",
-		},
-	}
-
-	createdToken, err := nestedtoken.Create(payload, privateKey)
-	if err != nil {
-		t.Fatalf("Failed to create token: %v", err)
-	}
-	t.Logf("Created token: %s\n", createdToken)
-
-	decodedToken, err := nestedtoken.Decode(createdToken)
-	if err != nil {
-		t.Fatalf("Failed to decode token: %v", err)
-	}
-
-	// validate token
-	valid, err := nestedtoken.Validate(decodedToken, decodedToken)
-	if err != nil {
-		t.Fatalf("Failed to decode token: %v", err)
-	}
-	if valid != true {
-		t.Fatalf("Failed to validate token")
-	}
-
-}
-
-func TestExtendValidate(t *testing.T) {
-	// Generating a sample token for testing
-	privateKey := generateECDSAKey(t)
-
-	pubkey, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
-	if err != nil {
-		t.Fatalf("Failed to marshal public key: %v", err)
-	}
-
-	payload := &nestedtoken.Payload{
-		Ver: 1,
-		Iat: time.Now().Unix(),
-		Iss: &nestedtoken.IDClaim{
-			CN: "Workload A",
-			PK: pubkey,
-		},
-		Aud: &nestedtoken.IDClaim{
-			CN: "Workload B",
-			PK: []byte("audience_public_key"),
-		},
-		Sub: &nestedtoken.IDClaim{
-			CN: "Workload A",
-			PK: pubkey,
-		},
-		Data: map[string]interface{}{
-			"custom_claim": "custom_value",
-		},
-	}
-
-	createdToken, err := nestedtoken.Create(payload, privateKey)
-	if err != nil {
-		t.Fatalf("Failed to create token: %v", err)
-	}
-	t.Logf("Created token: %s\n", createdToken)
-
-	decodedToken, err := nestedtoken.Decode(createdToken)
-	if err != nil {
-		t.Fatalf("Failed to decode token: %v", err)
-	}
-
-	// Prepare a new payload
-	newPayload := &nestedtoken.Payload{
-		Ver: 2,
-		Iat: time.Now().Unix(),
-		Iss: &nestedtoken.IDClaim{
-			CN: "Workload B",
-			PK: pubkey,
-		},
-		Aud: &nestedtoken.IDClaim{
-			CN: "Workload C",
-			PK: []byte("audience_public_key"),
-		},
-		Data: map[string]interface{}{
-			"updated_claim": "updated_value",
-		},
-	}
-
-	// Extend the token
-	extendedToken, err := nestedtoken.Extend(decodedToken, newPayload, privateKey)
-	if err != nil {
-		t.Fatalf("Failed to extend token: %v", err)
-	}
-	t.Logf("Extended token: %s\n", extendedToken)
-
-	// Decode the extended token
-	decodedExtendedToken, err := nestedtoken.Decode(extendedToken)
-	if err != nil {
-		t.Fatalf("Failed to decode extended token: %v", err)
-	}
-
-	// validate token
-	valid, err := nestedtoken.Validate(decodedExtendedToken, decodedToken)
-	if err != nil {
-		t.Fatalf("Failed to decode token: %v", err)
-	}
-	if valid != true {
-		t.Fatalf("Failed to validate token")
-	}
-
-}
